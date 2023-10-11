67224bf8de70aacd657e3e09389484e945fd2062
Denis Sheviakov <saivrem@gmail.com>
2023-10-11 15:37:14 +0300

    Add new task Add new solution for existing task

---
+++ src/main/java/org/dustyRoom/tasks/core/CompileFile.java
@@ -0,0 +1,67 @@
+package org.dustyRoom.tasks.core;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 0811
+ * Собираем файл из кусочков.
+ * Считывать с консоли имена файлов.
+ * Каждый файл имеет имя: [someName].partN.
+ * <p>
+ * Например, Lion.avi.part1, Lion.avi.part2, ..., Lion.avi.part37.
+ * <p>
+ * Имена файлов подаются в произвольном порядке. Ввод заканчивается словом "end".
+ * В папке, где находятся все прочтенные файлы, создать файл без суффикса [.partN].
+ * <p>
+ * Например, Lion.avi.
+ * <p>
+ * В него переписать все байты из файлов-частей используя буфер.
+ * Файлы переписывать в строгой последовательности, сначала первую часть, потом вторую, ..., в конце - последнюю.
+ * Закрыть потоки.
+ */
+
+//TODO add Tests
+public class CompileFile {
+
+    private final static String END = "end";
+
+    public static void main(String[] args) {
+        mySolution();
+    }
+
+    public static void mySolution() {
+        List<String> fileParts = new ArrayList<>();
+        Scanner console = new Scanner(System.in);
+        while (console.hasNextLine()) {
+            String line = console.nextLine();
+            if (END.equals(line)) {
+                break;
+            }
+            fileParts.add(line);
+        }
+
+        String fileName = fileParts.get(0);
+        fileName = fileName.substring(0, fileName.indexOf(".part"));
+
+        try (OutputStream os = new FileOutputStream(fileName)) {
+            fileParts.stream()
+                    .sorted()
+                    .forEach(part -> write(part, os));
+        } catch (IOException ignore) {
+        }
+    }
+
+    private static void write(String file, OutputStream os) {
+        try (InputStream is = new FileInputStream(file)) {
+            byte[] buff = new byte[65535];
+            int bytesRead;
+            while ((bytesRead = is.read(buff)) != -1) {
+                os.write(buff, 0, bytesRead);
+            }
+        } catch (IOException ignored) {
+        }
+    }
+}
--- src/main/java/org/dustyRoom/tasks/syntax/RemoveSameLines.java
+++ src/main/java/org/dustyRoom/tasks/syntax/RemoveSameLines.java
@@ -4,13 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 
+/**
+ * task0508
+ * <p>
+ * В этой задаче тебе нужно:
+ * <p>
+ * Считать 6 строк и заполнить ими массив strings.
+ * Удалить повторяющиеся строки из массива strings, заменив их на null (null должны быть не строками "null").
+ */
 @Slf4j
 public class RemoveSameLines {
 
     public static void solution(String[] lines) {
-        mySolution(lines);
+        flagSolution(lines);
     }
-    private static void mySolution(String[] lines) {
+
+    //TODO found task description, update test and solution
+    private static void tempVarSolution(String[] lines) {
         log.debug("Initial state: {}", Arrays.toString(lines));
         for (int i = 0; i < lines.length; i++) {
             String current = lines[i];
@@ -23,5 +33,23 @@ public class RemoveSameLines {
             }
         }
         log.debug("Modified array: {}", Arrays.toString(lines));
+    }
+
+    private static void flagSolution(String[] lines) {
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i] == null) {
+                continue;
+            }
+            boolean found = false;
+            for (int j = i + 1; j < lines.length; j++) {
+                if (lines[i].equals(lines[j])) {
+                    lines[j] = null;
+                    found = true;
+                }
+            }
+            if (found) {
+                lines[i] = null;
+            }
+        }
     }
 }
